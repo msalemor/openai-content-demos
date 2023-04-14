2a840252efb7db2b7fd52a3c5a4318daa286be47
Alex Morales <alemor@microsoft.com>
2023-04-14 14:13:45 -0400

    removed duplicate folder

--- openai-demos/server/run.sh
+++
@@ -1,9 +0,0 @@
-cd ../openai-demos
-sh deploy.sh
-cd ../server
-#rm server
-go run .
-#docker login
-#docker build . -t am8850/openaidemos:d-1000
-#docker image push am8850/openaidemos:dev
-#docker run --rm -p 3000:3000 --env-file .env am8850/openaidemos:dev
